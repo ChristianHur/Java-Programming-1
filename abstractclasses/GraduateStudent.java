bc8c76c58bc3343977ee2eb0770221f0934bd352
Christian Hur <chrisvhur@gmail.com>
2021-11-21 19:10:28 -0600

    Add files via upload

---
+++ abstractclasses/GraduateStudent.java
@@ -0,0 +1,23 @@
+package com.christianhur.inheritance;
+
+public class GraduateStudent extends Student {
+	private int cost = 15000;
+	private String level = "Graduate";
+
+	public GraduateStudent(int sid, String lastName, String firstName) {
+		super(sid, lastName, firstName);
+		this.setTuition(this.cost);
+		this.setType(this.level);
+	}
+
+	@Override
+	public void setTuition(int tuition) {
+		this.tuition = this.cost;		
+	}
+
+	@Override
+	public void setType(String type) {
+		this.type = type;
+		
+	}
+}
---
+++ abstractclasses/UndergraduateStudent.java
@@ -0,0 +1,25 @@
+package com.christianhur.inheritance;
+
+public class UndergraduateStudent extends Student {
+	private int cost = 8500;
+	private String level = "Undergraduate";
+
+
+	public UndergraduateStudent(int sid, String lastName, String firstName) {
+		super(sid, lastName, firstName);
+		this.setTuition(this.cost);
+		this.setType(this.level);
+
+	}
+
+	@Override
+	public void setTuition(int tuition) {
+		this.tuition = tuition;		
+	}
+	@Override
+	public void setType(String type) {
+		this.type = type;
+		
+	}
+
+}
